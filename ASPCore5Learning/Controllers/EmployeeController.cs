57f20fc530f52b2538e52b2ab1806a2905f6636c
vrnkmr111 <vrnkmr111@gmail.com>
2021-04-12 22:52:14 +0530

    adding core project

---
+++ ASPCore5Learning/Controllers/EmployeeController.cs
@@ -0,0 +1,37 @@
+﻿using BusinessLogicWebBridge.IBusinessLogicWebBridge;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPCore5Learning.Controllers
+{
+    [Route("Employee")]
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployeeWebBridge _employeeWebBridge;
+
+        public EmployeeController(IEmployeeWebBridge employeeWebBridge)
+        {
+            _employeeWebBridge = employeeWebBridge;
+        }
+        public ViewResult Index()
+        {
+            return View();
+        }
+        [Route("Employees")]
+        public ViewResult EmployeeList(int? id)
+        { 
+            var employees = _employeeWebBridge.GetAllEmployees();
+            return View("~/Views/Employee/Employees.cshtml", employees);
+        }
+        [Route("{Id}")]
+        public ViewResult EmployeeById(int Id)
+        {
+            var employees = _employeeWebBridge.GetEmployeeById(Id);
+            return View(employees);
+        }
+
+    }
+}
---
+++ ASPCore5Learning/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASPCore5Learning.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
